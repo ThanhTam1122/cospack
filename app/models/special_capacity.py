cdf1b3b4088e26e9c896b69fa4426a0dba536a9b
Kosuke Wada <34668860+worder1055@users.noreply.github.com>
2025-05-21 11:20:44 +0900

    Add : データベースの確認

--- app/models/special_capacity.py
+++ app/models/special_capacity.py
@@ -14,13 +14,13 @@ class SpecialCapacity(Base):
     HANMA48001 = Column("HANMA48001", CHAR(2), primary_key=True, nullable=False)
 
     # 2. 限度才数 - Capacity Limit (DECIMAL(10,0))
-    HANMA48002 = Column("HANMA48002", DECIMAL(8, 0), nullable=True)
+    HANMA48002 = Column("HANMA48002", DECIMAL(8, 0), nullable=True) #todo not null。 出荷日にして、運送会社コードとともに複合プライマリーキーにしてください。
 
     # 3. 最大重量（kg） - Max Weight (DECIMAL(10,0))
-    HANMA48003 = Column("HANMA48003", DECIMAL(10, 0), nullable=True)
+    HANMA48003 = Column("HANMA48003", DECIMAL(10, 0), nullable=True) #todo not null。　こちらが限度才数です
 
     # 4. 才数換算重量（kg） - Weight per Volume (DECIMAL(3,0))
-    HANMA48004 = Column("HANMA48004", DECIMAL(10, 0), nullable=True)
+    HANMA48004 = Column("HANMA48004", DECIMAL(10, 0), nullable=True) #todo not null。　こちらが最大重量です
 
     # 5. 更新番号 - Update Number (DECIMAL(9,0), starts at 0, auto +1)
     HANMA48999 = Column("HANMA48999", DECIMAL(9, 0), nullable=False, default=0)
